69afdddc37316627a80dccef7af87d29d85254ab
suntoes <mar26xd@gmail.com>
2022-10-03 12:42:33 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+
+import { Box, Container, Input, Stack, Heading, Button } from "@chakra-ui/react"
 
 function App() {
+  const [input, setInput] = useState("0")
+  const [guessNumber, setGuessNumber] = useState(-1)
+  const [announce, setAnnounce] = useState("Click play to start")
+
+  const newGame = () => {
+    const min = 0
+    const max = 100
+    const num = Math.floor(Math.random() * (max - min + 1) + min)
+    setInput("0")
+    setGuessNumber(num)
+    setAnnounce("Guess the number")
+  }
+
+  const inputHandler = (e) => {
+    setInput(e.target.value)
+    const parsed = parseFloat(e.target.value)
+    switch(true) {
+      case parsed > guessNumber:
+        setAnnounce(`${parsed} is high`)
+        break;
+      case parsed < guessNumber:
+        setAnnounce(`${parsed} is low`)
+        break;
+      case parsed === guessNumber:
+        setAnnounce(`Bingo! ${parsed} is the number`)
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box display="flex" position="fixed" boxSize="full">
+      <Container alignSelf="center">
+        <Stack>
+          <Heading>
+            {announce}
+          </Heading>
+          <Input 
+            type="number"
+            min="0"
+            max="100"
+            value={input}
+            disabled={guessNumber === -1 || guessNumber === parseFloat(input)}
+            onChange={inputHandler}
+            />
+          <Button
+            onClick={newGame}>
+            {
+              announce === "Click play to start"
+              ? "Play"
+              : guessNumber !== parseFloat(input)
+                ? "Reset"
+                : "Play again"
+            }
+          </Button>
+        </Stack>
+      </Container>
+    </Box>
   );
 }
 
